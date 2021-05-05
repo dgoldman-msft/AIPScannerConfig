8790b3e69284b6cacd91d002207e70af3156b955
Dave Goldman <39909564+dgoldman-msft@users.noreply.github.com>
2021-05-04 21:57:57 -0400

    Pester fixes

--- AIPScannerConfig/functions/Add-AccountToSQLRole.ps1
+++ AIPScannerConfig/functions/Add-AccountToSQLRole.ps1
@@ -6,10 +6,16 @@
     .DESCRIPTION
         This method will add the AIP Scanner account to the SQL dbcreator role
 
+    .PARAMETER UserDefinedSqlInstance
+        User specified SQL Instance
+
     .PARAMETER EnableException
         Depending on whether $EnableException is true or false it will do the following:
             1. ($True) - Throw a bloody terminating error. Game over.
             2. ($False) - Write a nice warning about how Foo failed bar, then terminate the function. The return on the next line will then end the calling function.
+
+    .EXAMPLE
+        PS C:\> Add-AccountToSQLRole -UserDefinedSqlInstance "Server01\DatabaseName"
 
     .EXAMPLE
         PS C:\> Add-AccountToSQLRole -EnableException
@@ -20,6 +26,9 @@
 
     [CmdletBinding(SupportsShouldProcess)]
     param (
+        [string]
+        $UserDefinedSqlInstance,
+
         [switch]
         $EnableException
     )
@@ -32,7 +41,12 @@
         try {
             Write-PSFMessage -Level Verbose -String 'Add-AccountToSQLRole.Message2'
             $null = [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SMO')
-            $sqlServer = New-Object 'Microsoft.SqlServer.Management.Smo.Server' "$env:COMPUTERNAME\SQLEXPRESS" -ErrorAction Stop
+            if ($UserDefinedSqlInstance) {
+                $sqlServer = New-Object 'Microsoft.SqlServer.Management.Smo.Server' $UserDefinedSqlInstance -ErrorAction Stop
+            }
+            else {
+                $sqlServer = New-Object 'Microsoft.SqlServer.Management.Smo.Server' "$env:COMPUTERNAME\SQLEXPRESS" -ErrorAction Stop
+            }
             Write-PSFMessage -Level Host -String 'Add-AccountToSQLRole.Message3'
             $login = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Login -ArgumentList $sqlServer, "$env:COMPUTERNAME\AIPScanner" -ErrorAction Stop
             $login.LoginType = "WindowsUser"
